da3ba72b47010b59674900c7732a95bd6f61c3f1
2021698 <rvanbeek@roc-dev.com>
2016-10-11 10:24:16 +0200

    eerste commit

--- src/h02/Show.java
+++ src/h02/Show.java
@@ -3,5 +3,32 @@ package h02;
 /**
  * Created by Rik Van Beek ict 2 on 11-10-2016.
  */
-public class Show {
+/*
+
+@(#)Show.java 1.0 03/02/26
+
+*
+
+Voorbeeld 2.1
+
+*
+
+*/
+
+//importeer de benodigde klassen uit de Java bibliotheek
+import java.awt.*;
+import java.applet.*;
+
+//een klasse met de naam Show van het type Applet
+public class Show extends Applet {
+
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+
+    }
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+        g.drawString("Welcome to Java!!", 50, 60 );
+    }
+
 }
