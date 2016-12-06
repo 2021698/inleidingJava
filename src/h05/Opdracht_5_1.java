c659f70795eb7bb03b3461470a213369ea820bc4
Rik van Beek <rvanbeek@roc-dev.com>
2016-12-06 16:14:50 +0100

    commit

--- src/h05/Opdracht_5_1.java
+++ src/h05/Opdracht_5_1.java
@@ -14,13 +14,13 @@ public class Opdracht_5_1 extends Applet {
     int hoogte = 40;
     int x = 700;
     int y = 220;
-    int cirkel = 0;
+    int cirkel = 785;
     int getBreedte = 0;
     int getHoogte = 150;
     int getGetBreedte = 0;
     int getGetHoogte = 0;
-    int round = 15;
-    int g1 = 45;
+    int round = 30;
+    int g1 = 0;
     int g2 = 45;
     int g3 = 0;
     int g4 = 360;
@@ -32,21 +32,28 @@ public class Opdracht_5_1 extends Applet {
 
     @Override
     public void paint(Graphics g) {
+        //line
         g.drawLine(height,height,width,height);
-
+        //rechthoek
         g.drawRect(height, hoogte, width, getHoogte);
-
+        //afgeronde rechthoek
         g.drawRoundRect(height,y,width,getHoogte,round,round);
-
+        //gevulde rechthoek met ovaal
+        g.setColor(new Color(255, 0, 255));
         g.fillRect(breedte, hoogte, width, getHoogte);
+        g.setColor(Color.black);
         g.drawArc(breedte, hoogte, width, getHoogte, g3, g4);
-
+        //gevulde ovaal
+        g.setColor(new Color(255, 0, 255));
         g.fillArc(breedte, y, width, getHoogte, g3, g4);
-
-        g.fillArc(breedte, x, width, getHoogte, g3, g4);
-        g.drawArc(breedte, x, width, getHoogte, g1, g2);
-
-        g.drawArc(breedte, x, width, getHoogte, g3, g4);
+        //taartpunt met ovaal eromheen
+        g.setColor (Color.black);
+        g.drawArc(x, hoogte, width, getHoogte, g3, g4);
+        g.setColor(new Color(225, 0, 255));
+        g.fillArc(x, hoogte, width, getHoogte, g1, g2);
+        //cirkel
+        g.setColor (Color.black);
+        g.drawArc(cirkel, y, getHoogte, getHoogte, g4, g4);
 
     }
 }
--- src/h05/Opdracht_5_2.java
+++ src/h05/Opdracht_5_2.java
@@ -8,14 +8,68 @@ import java.awt.*;
  */
 public class Opdracht_5_2 extends Applet {
 
+        int valerie;
+        int jeroen;
+        int hans;
+        int base;
+        int a10 = 10;
+        int a20 = 20;
+        int a30 = 30;
+        int a40 = 40;
+        int a50 = 50;
+        int a60 = 60;
+        int a70 = 70;
+        int a80 = 80;
+        int a90 = 90;
+        int a100 = 100;
 
     @Override
     public void init() {
-        super.init();
+        //gewichten
+        valerie = 40;
+        jeroen = 100;
+        hans = 80;
+        //berekening
+        base = 320;
     }
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
+
+        //ylijn
+        g.drawLine(60, 20, 60, 320 );
+
+        //xlijn
+        g.drawLine(60, 320, 400, 320 );
+
+        //getallen
+        g.drawString("00", 40, 320 );
+        g.drawLine(40, 320, 60, 320 );
+        g.drawString("20", 40, 259 );
+        g.drawLine(40, 260, 60, 260 );
+        g.drawString("40", 40, 199 );
+        g.drawLine(40, 200, 60, 200 );
+        g.drawString("60", 40, 139 );
+        g.drawLine(40, 140, 60, 140 );
+        g.drawString("80", 40, 79 );
+        g.drawLine(40, 80, 60, 80 );
+        g.drawString("100", 40, 19 );
+        g.drawLine(40, 20, 60, 20 );
+
+        //Auto tabellen (change int)
+        g.drawString("Valerie", 90,360 );
+        g.setColor(Color.yellow);
+        g.fillRect(60,base - valerie * 3, 100, valerie * 3);
+
+        g.setColor(Color.black);
+        g.drawString("Jeroen", 190, 360 );
+        g.setColor(Color.blue);
+        g.fillRect(160,base - jeroen * 3, 100, jeroen * 3);
+
+        g.setColor (Color.black);
+        g.drawString("Hans", 290, 360 );
+        g.setColor(Color.red);
+        g.fillRect(260,base - hans * 3, 100, hans * 3);
     }
+
 }
