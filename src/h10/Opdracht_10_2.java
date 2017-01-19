9723379c18cc1a7c730cb3e8fc729c6c0ff96d6c
Rik van Beek <rvanbeek@roc-dev.com>
2017-01-19 12:22:17 +0100

    commit

---
+++ src/h10/Opdracht_10_2.java
@@ -0,0 +1,63 @@
+package h10;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ * Created by Rik van Beek on 19-1-2017.
+ */
+public class Opdracht_10_2 {
+
+
+    /**
+     * Created by Rik van Beek on 5-1-2017.
+     */
+    public class Opdracht_10_1 extends Applet {
+        TextField tekstvak;
+        Label label;
+        String s, tekst;
+        double oud;
+        double hoogste = 0;
+        double invoer;
+        double laagste;
+        Button knop;
+
+        @Override
+        public void init() {
+            tekstvak = new TextField("", 20);
+            label = new Label("getal");
+            knop = new Button("knop");
+            tekstvak.addActionListener(new TekstvakListerner());
+            knop.addActionListener(new TekstvakListerner());
+            tekst = "";
+            add(label);
+            add(tekstvak);
+            add(knop);
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            g.drawString("" + hoogste, 50, 50);
+            g.drawString("" + oud, 50, 80);
+            g.drawString("" + laagste,50,110);
+        }
+
+        class TekstvakListerner implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                s = tekstvak.getText();
+                invoer = Double.parseDouble(s);
+                if (invoer > hoogste) {
+                    hoogste = invoer;
+                } else if (invoer < hoogste) {
+                    laagste = invoer;
+                }
+                oud = invoer;
+                repaint();
+            }
+        }
+    }
+}
