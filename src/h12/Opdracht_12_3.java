b0e9733e00d9321e7836a376067576131fb66716
Rik van Beek <2021698@talnet.nl>
2017-04-21 12:47:53 +0200

    .

--- src/h12/Opdracht_12_3.java
+++ src/h12/Opdracht_12_3.java
@@ -1,32 +1,57 @@
 package h12;
 
-import java.applet.Applet;
 import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+import java.util.*;
 
 /**
  * Created by Rik van Beek on 14-2-2017.
  */
 public class Opdracht_12_3 extends Applet {
-    double Tekstvak[];
-    TextField tekstvak[];
+    int[] getallen;
+    TextField[] tekstvakken;
+    int[] sort;
+    Button knop;
 
     public void init() {
-        Tekstvak = new double[5];
-        tekstvak = new TextField[5];
+        getallen = new int[5];
+        tekstvakken = new TextField[5];
+        knop = new Button("OK");
+        add(knop);
 
-        for (int teller = 0; teller < Tekstvak.length; teller++) {
-            Tekstvak[teller] = 5 * teller + 1;
-            tekstvak[teller] = new TextField();
-            add(tekstvak[teller]);
+        for (int teller = 0; teller < getallen.length; teller++) {
+            tekstvakken[teller] = new TextField();
+            add(tekstvakken[teller]);
         }
-        repaint();
+        knop.addActionListener(new TextvakListener());
+
     }
 
     @Override
     public void paint(Graphics g) {
-        for (int teller = 0; teller < Tekstvak.length; teller ++) {
-
+        int y = 50;
+        int x = 50;
+        for (int teller = 0; teller < getallen.length; teller++) {
+            g.drawString("" + getallen[teller], x, y);
+            y += 20;
         }
-        repaint();
+    }
+
+    class TextvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            for (int teller = 0; teller < tekstvakken.length; teller++) {
+                String s = tekstvakken[teller].getText();
+                int getal = Integer.parseInt(s);
+                getallen[teller] = getal;
+            }
+
+            Arrays.sort(getallen);
+
+            for (int teller = 0; teller < getallen.length; teller++) {
+                tekstvakken[teller].setText("" + getallen[teller]);
+            }
+            repaint();
+        }
     }
 }
---
+++ src/h12/Opdracht_12_5.java
@@ -0,0 +1,7 @@
+package h12;
+
+/**
+ * Created by Rik van Beek on 21-4-2017.
+ */
+public class Opdracht_12_5 {
+}
