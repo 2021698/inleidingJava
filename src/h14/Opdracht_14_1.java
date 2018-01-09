1ffa87b23b77138c4ae860dc0909b81b31d377c7
Rik van Beek <2021698@talnet.nl>
2018-01-09 10:07:04 +0100

    .

---
+++ src/h14/Opdracht_14_1.java
@@ -0,0 +1,22 @@
+package h14;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by Rik van Beek on 09/01/2018.
+ */
+public class Opdracht_14_1 extends Applet {
+
+    String[] kaartsoort = {"Klaver","Harten","Schoppen","Ruiten"};
+    String[] kaartnummer = {"A","1","2","3","4","5","6","7","8","9","10","Boer","Vrouw","Heer"};
+
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
