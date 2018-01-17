2e0d31f29ab7be1f725934b370b984f79b3952c2
Rik van Beek <2021698@talnet.nl>
2018-01-17 16:15:51 +0100

    .

--- src/h14/Opdracht_14_1.java
+++ src/h14/Opdracht_14_1.java
@@ -8,15 +8,24 @@ import java.awt.*;
  */
 public class Opdracht_14_1 extends Applet {
 
+    double random;
+    int worp;
+    private int gedeeldekaart;
+
+
     String[] kaartsoort = {"Klaver","Harten","Schoppen","Ruiten"};
     String[] kaartnummer = {"A","1","2","3","4","5","6","7","8","9","10","Boer","Vrouw","Heer"};
 
-
     public void init() {
 
+        random = Math.random();
+        worp = (int)(random*52+1);
+        kaartsoort = new String[4];
+        kaartnummer = new String[14];
+        gedeeldekaart = kaartsoort + kaartnummer;
     }
 
     public void paint(Graphics g) {
-
+        g.drawString("gedeelde kaart: " + gedeeldekaart,50,50);
     }
 }
