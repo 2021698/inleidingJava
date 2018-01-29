078c1921e27dc8d1b3603492545c2a31e5d59a48
Rik van Beek <2021698@talnet.nl>
2018-01-29 09:27:30 +0100

    .

---
+++ src/h14/Opdracht_14_2.java
@@ -0,0 +1,18 @@
+package h14;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by Rik van Beek on 25/01/2018.
+ */
+public class Opdracht_14_2 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
