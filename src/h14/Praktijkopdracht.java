e7292711bacaa3f7b4fdae188f6ba6bf5a59bbef
Rik van Beek <2021698@talnet.nl>
2018-03-28 16:15:02 +0200

    .

--- src/h14/Praktijkopdracht.java
+++ src/h14/Praktijkopdracht.java
@@ -1,5 +1,6 @@
 package h14;
 
+import javax.swing.*;
 import java.applet.*;
 import java.awt.*;
 import java.lang.reflect.Array;
@@ -15,10 +16,21 @@ public class Praktijkopdracht extends Applet {
 
     private int x1;
     private int y1;
+    int smiley = 23;
+
+    Button knop;
+    TextField tekstvak;
+    Label label;
 
     String[] cijfers = {"1", "2", "3"};
 
     public void init() {
+        knop = new Button();
+        knop.setLabel("Speel");
+        label = new Label("Hoeveel smileys neem je (één, twee of drie)?");
+        tekstvak = new TextField("");
+        add(label);
+        add(tekstvak);
         pad = Praktijkopdracht.class.getResource("images/");
         afbeelding = getImage(pad, "basic1-119_smiley_neutral-512_40px.png");
 
@@ -27,7 +39,8 @@ public class Praktijkopdracht extends Applet {
     public void paint(Graphics g) {
         x1 = 0;
         y1 = 0;
-        for (int i = 0; i < 23; i++) {
+        g.drawString("Aantal smileys: " + smiley + ". Jouw beurt.", 20, 275);
+        for (int i = 0; i < smiley; i++) {
             x1 += 40;
             g.drawImage(afbeelding, x1, y1, 40, 40, this);
             if (i % 4 == 3) {
@@ -36,4 +49,5 @@ public class Praktijkopdracht extends Applet {
             }
         }
     }
+
 }
